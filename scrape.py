c3a956fff061a48ca2aa2b513dbbd982084b3e9a
unknown <mnaveenkmr3096@gmail.com>
2023-02-12 19:39:39 +0530

    upload

---
+++ scrape.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = 'https://www.imdb.com/chart/top/'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+
+movies = soup.select('tbody.lister-list tr')
+
+
+all_movies = []
+
+for i in range(0,len(movies)):
+    movie = movies[i]
+    rank = i+1
+    title = movie.select_one('td.titleColumn a').get_text().strip()
+    year = movie.select_one('td.titleColumn span.secondaryInfo').get_text().strip()
+    rating = movie.select_one('td.posterColumn span[name="ir"]').attrs.get('data-value')
+    crew = movie.select_one('td.titleColumn a').attrs.get('title')
+    all_movies.append([rank, title, year, rating, crew])
+    print(all_movies)
+    #print(f"{rank}. {title} {year} {rating} {crew}")
+
+
+df = pd.DataFrame(all_movies,columns=['Rank', 'Title', 'Year', 'IMDB Rating', 'Crew'])
+df.to_excel('movies.xlsx',index=False)
+
